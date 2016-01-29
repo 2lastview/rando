eb33da0e56ce45c59cf80b2145b0decefc44af33
Moritz Tomasi <moritz@zoomsquare.com>
2016-01-29 17:34:51 +0100

    add(config): config and config read/valdation added.

--- src/rando.py
+++ src/rando.py
@@ -1,7 +1,46 @@
-import time
+import ConfigParser
+from ConfigParser import SafeConfigParser
+import monitor
+
 
 def main():
-    print "Hello"
+    # init parser
+    parser = SafeConfigParser()
+    parser.read('../config/api.ini')
+
+    # get API entry
+    if parser.has_section('API'):
+        try:
+            base_url = parser.get('API', 'base_url')
+            testing_key = parser.get('API', 'testing_key')
+            production_key = parser.get('API', 'production_key')
+        except ConfigParser.NoOptionError as e:
+            print ">> Something missing in API config: " + e.message
+            return
+    else:
+        print ">> API section missing in config"
+        return
+
+    # loop over config and pass oeffis to workers
+    parser = SafeConfigParser()
+    parser.read('../config/oeffis.ini')
+    for section_name in parser.sections():
+        # must be in config
+        try:
+            name = parser.get(section_name, "name")
+            rbl = parser.get(section_name, "rbl")
+        except ConfigParser.NoOptionError as e:
+            print ">> Something missing in " + section_name + " config:" + e.message
+            return
+
+        # start worker
+        print base_url
+        print testing_key
+        print production_key
+        print name
+        print rbl
+
+        monitor.worker()
 
 if __name__ == '__main__':
     main()
