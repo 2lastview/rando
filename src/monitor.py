f39cc94f72ac41ea2c36cb3dd54fe4fd770dda44
Moritz Tomasi <moritz@zoomsquare.com>
2016-01-29 23:27:47 +0100

    add(threading): basic threading mechanisms added.

--- src/monitor.py
+++ src/monitor.py
@@ -2,5 +2,31 @@ import threading
 import time
 
 
-def worker():
-    print "Hello"
+exitFlag = False
+
+
+class Monitor(threading.Thread):
+
+    def __init__(self, base_url, testing_key, production_key, name, rbl, *args):
+        # init threading
+        threading.Thread.__init__(self)
+        # init params
+        self.base_url = base_url
+        self.testing_key = testing_key
+        self.production_key = production_key
+        self.name = name
+        self.rbl = rbl
+        self.args = args
+
+    def run(self):
+        print "Starting thread for " + self.name
+        self.get_monitor_data(self.name, 3)
+        print "Exiting thread for " + self.name
+
+    @staticmethod
+    def get_monitor_data(thread_name, delay):
+        while True:
+            if exitFlag:
+                thread_name.exit()
+            time.sleep(delay)
+            print "Calling API " + thread_name
--- src/rando.py
+++ src/rando.py
@@ -1,6 +1,6 @@
 import ConfigParser
 from ConfigParser import SafeConfigParser
-import monitor
+from monitor import Monitor
 
 
 def main():
@@ -15,30 +15,34 @@ def main():
             testing_key = parser.get('API', 'testing_key')
             production_key = parser.get('API', 'production_key')
         except ConfigParser.NoOptionError as e:
-            print ">> Something missing in API config: " + e.message
+            print "Something missing in API config: " + e.message
             return
     else:
-        print ">> API section missing in config"
+        print "API section missing in config"
         return
 
     # loop over config and pass oeffis to workers
     parser = SafeConfigParser()
     parser.read('../config/oeffis.ini')
+    threads = []
     for section_name in parser.sections():
         # must be in config
         try:
             name = parser.get(section_name, "name")
             rbl = parser.get(section_name, "rbl")
         except ConfigParser.NoOptionError as e:
-            print ">> Something missing in " + section_name + " config:" + e.message
-            return
+            print "Something missing in " + section_name + " config:" + e.message
+            continue
 
-        # start worker
-        print base_url
-        print testing_key
-        print production_key
-        print name
-        print rbl
+        # threading
+        thread = Monitor(base_url, testing_key, production_key, name, rbl)
+        thread.start()
+        threads.append(thread)
+
+    for t in threads:
+        t.join()
+
+    print "Exiting rando. All threads stopped."
 
 
 if __name__ == '__main__':
