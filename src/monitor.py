9fd8a713df284077fa5e813d65ab4c6a3268d5a6
Moritz Tomasi <moritz@zoomsquare.com>
2016-01-29 23:42:01 +0100

    improve(threading)

--- src/monitor.py
+++ src/monitor.py
@@ -2,14 +2,13 @@ import threading
 import time
 
 
-exitFlag = False
-
-
 class Monitor(threading.Thread):
 
     def __init__(self, base_url, testing_key, production_key, name, rbl, *args):
         # init threading
         threading.Thread.__init__(self)
+        # exit flag
+        self.exit_flag = False
         # init params
         self.base_url = base_url
         self.testing_key = testing_key
@@ -23,10 +22,10 @@ class Monitor(threading.Thread):
         self.get_monitor_data(self.name, 3)
         print "Exiting thread for " + self.name
 
-    @staticmethod
-    def get_monitor_data(thread_name, delay):
+    def get_monitor_data(self, thread_name, delay):
         while True:
-            if exitFlag:
-                thread_name.exit()
+            if self.exit_flag:
+                break
             time.sleep(delay)
             print "Calling API " + thread_name
+            self.exit_flag = True
